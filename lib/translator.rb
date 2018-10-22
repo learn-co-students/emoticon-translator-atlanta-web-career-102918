e5ed67371f47f0ddd16d4d1ffb981654afa20dd4
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2018-10-22 09:22:50 -0400

    emoticon commit

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,41 @@
 # require modules here
+require "yaml"
+require "pry"
 
-def load_library
+
+def load_library(filename)
   # code goes here
+  meaning = {}
+  emoticon = {}
+
+  combined = {"get_meaning" => meaning, "get_emoticon" => emoticon}
+  hash = YAML.load_file(filename)
+
+  hash.each do |english_word, emoticons|
+    combined["get_meaning"][emoticons[1]] = english_word;
+    combined["get_emoticon"][emoticons[0]] = emoticons[1]
+  end
+  return combined
 end
 
-def get_japanese_emoticon
+
+
+def get_japanese_emoticon(filepath, emoticon)
   # code goes here
+  temp = load_library(filepath)["get_emoticon"][emoticon]
+  if temp == nil
+    return "Sorry, that emoticon was not found"
+  else
+    return temp
+  end
 end
 
-def get_english_meaning
+def get_english_meaning(filepath, emoticon)
   # code goes here
+  temp = load_library(filepath)["get_meaning"][emoticon]
+  if temp == nil
+    return "Sorry, that emoticon was not found"
+  else
+    return temp
+  end
 end
