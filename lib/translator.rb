b563178d00d5e211b928a5dc80b3dd0f22dfb6cf
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2018-10-22 09:32:34 -0400

    third try

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,18 +5,14 @@ require "pry"
 def load_library(filename)
   meaning = {}
   emoticon = {}
-
   combined = {"get_meaning" => meaning, "get_emoticon" => emoticon}
   hash = YAML.load_file(filename)
-
   hash.each do |english_word, emoticons|
     combined["get_meaning"][emoticons[1]] = english_word;
     combined["get_emoticon"][emoticons[0]] = emoticons[1]
   end
   return combined
 end
-
-
 
 def get_japanese_emoticon(filepath, emoticon)
   temp = load_library(filepath)["get_emoticon"][emoticon]
